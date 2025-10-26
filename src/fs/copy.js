544ec7f6a2d5740edef89024e617502c2bd3ff3d
Liuba-K <kundasl1@gmail.com>
2025-10-26 08:45:11 +0300

    feat: implement copy folder

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,18 @@
+import { cp, access } from "node:fs/promises";
+
 const copy = async () => {
-  // Write your code here
+  try {
+    await access("files");
+    try {
+      await access("files_copy");
+      throw new Error("FS operation failed");
+    } catch (err) {
+      if (err.code !== "ENOENT") throw err;
+    }
+    await cp("files", "files_copy", { recursive: true });
+  } catch (err) {
+    console.error("FS operation failed");
+  }
 };
 
 await copy();
