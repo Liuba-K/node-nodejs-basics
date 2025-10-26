d70e8a7d0a9e308b5f3dd3949af00fc842cc575f
Liuba-K <kundasl1@gmail.com>
2025-10-26 23:49:27 +0300

    feat: implement prints content of file

--- src/fs/list.js
+++ src/fs/list.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "node:url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const folderPath = path.join(__dirname, "filesz");
+const folderPath = path.join(__dirname, "files");
 
 const list = async () => {
   try {
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,14 @@
+import { readFile } from "node:fs/promises";
+import path from "node:path";
+
 const read = async () => {
-  // Write your code here
+  try {
+    const filePath = path.join(import.meta.dirname, "files", "fileToRead.txt");
+    const content = await readFile(filePath, { encoding: "utf8" });
+    console.log(content);
+  } catch (Error) {
+    console.error("FS operation failed");
+  }
 };
 
 await read();
