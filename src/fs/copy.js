fcb48aab6fdbd81da7de325fc71c4caa0ba8d13a
Liuba-K <kundasl1@gmail.com>
2025-10-26 15:54:15 +0300

    feat: implement rename file

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -3,12 +3,14 @@ import { cp, access } from "node:fs/promises";
 const copy = async () => {
   try {
     await access("files");
+
     try {
       await access("files_copy");
       throw new Error("FS operation failed");
     } catch (err) {
       if (err.code !== "ENOENT") throw err;
     }
+
     await cp("files", "files_copy", { recursive: true });
   } catch (err) {
     console.error("FS operation failed");
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const oldPath = path.join(__dirname, "files", "wrongFilename.txt");
+const newPath = path.join(__dirname, "files", "properFilename.md");
+
 const rename = async () => {
-  // Write your code here
+  fs.access(oldPath, (Error) => {
+    if (Error) {
+      console.error("FS operation failed");
+      return;
+    }
+
+    fs.access(newPath, (Error) => {
+      if (!Error) {
+        console.error("FS operation failed");
+        return;
+      }
+
+      fs.rename(oldPath, newPath, (Error) => {
+        if (Error) {
+          console.error("FS operation failed");
+          return;
+        }
+      });
+    });
+  });
 };
 
 await rename();
