6f7fdbb2d2d07940ba51f9bf9281db60ca5c5502
Liuba-K <kundasl1@gmail.com>
2025-10-26 23:34:17 +0300

    feat: implement prints array of all filenames

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import { readdir, access } from "node:fs/promises";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const folderPath = path.join(__dirname, "filesz");
+
 const list = async () => {
-  // Write your code here
+  try {
+    await access(folderPath);
+    const files = await readdir(folderPath);
+    console.log(files);
+  } catch (Error) {
+    console.error("FS operation failed");
+  }
 };
 
 await list();
